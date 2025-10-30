a7b761a6c402cc2c67f3878a592247f4dffb092f
stabjana <garschtubald@gmail.com>
2025-10-30 14:26:38 +0200

    add app module to fix run the server

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from "@nestjs/common";
 import { AuthModule } from "./auth/auth.module";
-import { UserModule } from './user/user.module';
-import { PlaygroundModule } from './playground/playground.module';
+import { UserModule } from "./user/user.module";
+import { PlaygroundModule } from "./playground/playground.module";
+import { AppController } from "./app.contoller";
 
 @Module({
   imports: [AuthModule, UserModule, PlaygroundModule],
+  controllers: [AppController],
 })
 export class AppModule {}
