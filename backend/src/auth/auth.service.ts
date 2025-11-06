123c4050875beb0acfd6bdad5942583432fda49b
stabjana <garschtubald@gmail.com>
2025-11-06 15:55:37 +0200

    add simple user creation

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,13 +1,31 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto';
+import * as argon2 from 'argon2';
 
 @Injectable()
 export class AuthService {
   constructor(private prisma: PrismaService) {}
 
-  signUp(dto: AuthDto) {
-    return 'I am signed up';
+  async signUp(dto: AuthDto) {
+    // generate the password hash
+    const hash = await argon2.hash(dto.password);
+    // save the new user in db
+    const user = await this.prisma.user.create({
+      data: {
+        email: dto.email,
+        hash,
+      },
+      /* select: {
+        id: true,
+        email: true,
+        createdAt: true,
+      }, */
+      // that part will be handled with transformer
+    });
+
+    // return the saved user
+    return user;
   }
 
   signIn() {
