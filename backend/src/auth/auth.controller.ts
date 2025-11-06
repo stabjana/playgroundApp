8d54a5e152af53ca007ae80bd118b0aa4241c5e2
stabjana <garschtubald@gmail.com>
2025-11-06 13:15:24 +0200

    add prettier and ignore

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,17 +1,18 @@
 import { Body, Controller, Post } from "@nestjs/common";
 import { AuthService } from "./auth.service";
+import { AuthDto } from "./dto";
 
 @Controller("auth")
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post("signup")
-  signUp(@Body() dto: any) {
+  signUp(@Body() dto: AuthDto) {
     console.log({ dto });
-    // signUp(@Req() req: Request) {
-    // console.log(req.body); // req not needed for nest
     return this.authService.signUp();
   }
+  // instead of if (!email) throw error we can use class transformer and class-validator library
+  // pipe in nest js - functions that transform data
 
   @Post("signin")
   signIn() {
