17c10c118a6678b30914bd0094d1ffed06624f48
stabjana <garschtubald@gmail.com>
2025-11-10 16:04:53 +0200

    fixing environment

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto';
-import * as argon2 from 'argon2';
+// import * as argon2 from 'argon2';
 
 @Injectable()
 export class AuthService {
@@ -11,7 +11,7 @@ export class AuthService {
     // generate the password hash
     const hash = await argon2.hash(dto.password);
     // save the new user in db
-    const user = await this.prisma.user.create({
+    const user = await this.prisma.users.create({
       data: {
         email: dto.email,
         hash,
