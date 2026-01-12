20d5e029e4ae8a7f7a8d42cf74f4fdb731d61702
stabjana <garschtubald@gmail.com>
2026-01-12 21:15:19 +0200

    cleanup backend

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,12 +1,9 @@
 import { Injectable } from '@nestjs/common';
-import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto';
 // import * as argon2 from 'argon2';
 
 @Injectable()
 export class AuthService {
-  constructor(private prisma: PrismaService) {}
-
   async signIn(dto: AuthDto) {
     return 'I am signed in';
   }
